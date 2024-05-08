3eb08805c6df9d019eaf160d2ad3fa00b4901035
GALE Developer <71121453+EEthanShi@users.noreply.github.com>
2024-05-09 09:05:59 +1000

    Update wavelet_graph_kan_layer.py

--- wavelet_graph_kan_layer.py
+++ wavelet_graph_kan_layer.py
@@ -1,56 +1,44 @@
-import torch
-import torch.nn as nn
-
-import numpy as np
-
-    
 class NaiveWaveletKANLayer(nn.Module):
-    def __init__(self, inputdim, outdim, gridsize=300, addbias=True, num_scales=3):
+    def __init__(self, inputdim, outdim, gridsize=300, addbias=True):
         super(NaiveWaveletKANLayer, self).__init__()
         self.gridsize = gridsize
         self.addbias = addbias
         self.inputdim = inputdim
         self.outdim = outdim
-        self.num_scales = num_scales  # Number of different sigma values
+        #self.num_scales = num_scales  # Number of different sigma values
 
         # Initialize Fourier coefficients
-        self.fouriercoeffs = nn.Parameter(torch.randn(num_scales, outdim, inputdim, gridsize) / 
+        self.fouriercoeffs = nn.Parameter(torch.randn(outdim, inputdim, gridsize) /
                                           (np.sqrt(inputdim) * np.sqrt(self.gridsize)))
         
         # Initialize sigma parameters for each scale
-        self.sigmas = nn.Parameter(torch.ones(num_scales, 1, 1, gridsize))
+        self.sigmas = nn.Parameter(torch.ones(1, inputdim, gridsize))
+        self.locs = nn.Parameter(torch.randn(1, inputdim, gridsize))
 
         if self.addbias:
             self.bias = nn.Parameter(torch.zeros(1, outdim))
 
-    def mexican_hat_wavelet(self, x, sigma):
+    def mexican_hat_wavelet(self, x, loc, sigma):
         # Mexican hat wavelet function
-        sigma_x = x / sigma
+        sigma_x = (x - loc) / sigma
         return (2 / (torch.sqrt(torch.tensor(3)) * (torch.pi**0.25))) * (1 - sigma_x**2) * torch.exp(-sigma_x**2 / 2)
     
     def forward(self, x):
-        x = x.view(-1, self.inputdim)  #
-        xrshp = x.view(x.shape[0], 1, self.inputdim, 1)  
+        x = x.view(-1, self.inputdim)  #     N x d
+        xrshp = x.view(x.shape[0], self.inputdim, 1)   # N x d x 1
 
-        results = []
-        for i in range(self.num_scales):
-            wavelet_transform = self.mexican_hat_wavelet(xrshp, self.sigmas[i])  # Apply wavelet function and sum
-            results.append(wavelet_transform)
+        #for i in range(self.num_scales):
+        wavelet_transform = self.mexican_hat_wavelet(xrshp, self.locs, self.sigmas)  # Apply wavelet function in shape N x d x G  (G grid number)
         
-        results_reshape = []
-        for i in results: 
-            results_re = torch.reshape(i, (1, x.shape[0], x.shape[1], self.gridsize))
-            results_reshape.append(results_re)
         
-        concatenated_results = torch.cat(results_reshape, dim=0)
-        
-        y = torch.einsum("dbik,djik->bj", concatenated_results, self.fouriercoeffs)
+        y = torch.einsum("bdk,odk->bo", wavelet_transform, self.fouriercoeffs)   # N x o
 
         if self.addbias:
             y += self.bias
         
         y = y.view(-1, self.outdim)  # Reshape to original output shape
         return y
+    
     
     
     
